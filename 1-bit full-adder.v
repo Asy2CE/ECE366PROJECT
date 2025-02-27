9324038694363a34c6f0d4d816ca5982471ebe60
Asy2CE <masy2@uic.edu>
2025-02-26 21:26:31 -0600

    1-bit-full-adder

--- 1-bit full-adder.v
+++ 1-bit full-adder.v
@@ -3,7 +3,6 @@ module one_bit_full_adder(A, B, Cin, S, Cout);
     output reg S, Cout;
 
     always @(*) begin
-        // In behavioral modeling, we can simply use addition
         {Cout, S} = A + B + Cin;
     end
 endmodule
