b0e41f15de027150a8e4aa50b2db41db2ebd7ed6
ytiru3 <ytiru3@uic.edu>
2025-03-13 20:03:49 -0500

    test

--- testbench2.v
+++ testbench2.v
@@ -13,4 +13,13 @@ module tb_16bit_PPA;
         $monitor("Time=%0t | A=%h, B=%h, Cin=%b | S=%h, Cout=%b",
                  $time, A, B, Cin, S, Cout);
     
-    
+        A = 16'h0000; B = 16'h0000; Cin = 0; #10;
+        A = 16'h0001; B = 16'h0002; Cin = 0; #10;
+        A = 16'h0003; B = 16'h0003; Cin = 1; #10;
+        A = 16'h00F0; B = 16'h00F0; Cin = 0; #10;
+        A = 16'hFFFF; B = 16'h0001; Cin = 0; #10;
+        A = 16'hFFFF; B = 16'hFFFF; Cin = 1; #10;
+
+        $finish;
+    end
+endmodule
